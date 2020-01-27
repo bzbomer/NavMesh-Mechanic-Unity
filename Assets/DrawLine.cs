a6cd04ff9286ceb6468bdc6e3574a0449facce70
bzbomer <omerbozaba@gmail.com>
2020-01-27 03:10:58 +0300

    Upload project

---
+++ Assets/DrawLine.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour
+{
+
+    public GameObject linePrefab;
+    public GameObject currentLine;
+    public LineRenderer lineRenderer;
+    public EdgeCollider2D edgeCollider;
+    public List<Vector2> fingerPositions;
+    public Material lineMat;
+    int palm = 0;
+    public GameObject karakter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //lineMat.color = Color.blue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CreateLine();
+            palm++;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 tempFingerPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector2.Distance(tempFingerPos, fingerPositions[fingerPositions.Count - 1]) > .1f)
+            {
+                UpdateLine(tempFingerPos);
+            }
+        }
+        /*
+        switch (palm % 5) {
+            case 0: lineMat.color = Color.green; break;
+            case 1: lineMat.color = Color.red; break;
+            case 2: lineMat.color = Color.blue; break;
+            case 3: lineMat.color = Color.cyan; break;
+            case 4: lineMat.color = Color.magenta; break;
+        }*/
+    }
+
+    void CreateLine()
+    {
+        currentLine = Instantiate(linePrefab, Vector3.zero, Quaternion.identity);
+        lineRenderer = currentLine.GetComponent<LineRenderer>();
+        edgeCollider = currentLine.GetComponent<EdgeCollider2D>();
+        fingerPositions.Clear();
+        fingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        fingerPositions.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        lineRenderer.SetPosition(0, fingerPositions[0]);
+        lineRenderer.SetPosition(1, fingerPositions[1]);
+        edgeCollider.points = fingerPositions.ToArray();
+
+    }
+
+    void UpdateLine(Vector2 newFingerPos)
+    {
+        fingerPositions.Add(newFingerPos);
+        lineRenderer.positionCount++;
+        lineRenderer.SetPosition(lineRenderer.positionCount - 1, newFingerPos);
+        karakter.transform.position = new Vector2(newFingerPos.x, newFingerPos.y - 0.5f);
+        edgeCollider.points = fingerPositions.ToArray();
+
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform hedef;
+    NavMeshAgent agent;
+    public GameObject sp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        agent.SetDestination(hedef.position);
+        this.OnDrawGizmosSelected();
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+
+        var nav = GetComponent<NavMeshAgent>();
+        if (nav == null || nav.path == null)
+            return;
+
+        var line = this.GetComponent<LineRenderer>();
+        if (line == null)
+        {
+            line = this.gameObject.AddComponent<LineRenderer>();
+            line.material = new Material(Shader.Find("Sprites/Default")) { color = Color.yellow };
+            line.SetWidth(0.5f, 0.5f);
+            line.SetColors(Color.yellow, Color.yellow);
+        }
+
+        var path = nav.path;
+
+        line.SetVertexCount(path.corners.Length);
+
+        for (int i = 0; i < path.corners.Length; i++)
+        {
+            line.SetPosition(i, path.corners[i]);
+        }
+
+    }
+}
